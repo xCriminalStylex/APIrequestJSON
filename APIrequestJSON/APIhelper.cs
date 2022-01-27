bef16e5bf3f60d95701f1e07b2ecb0ef925e18b7
xCriminalStylex <oleksandrzikran@gmail.com>
2022-01-27 22:55:11 +0200

    Добавьте файлы проекта.

---
+++ APIrequestJSON/APIhelper.cs
@@ -0,0 +1,83 @@
+﻿using OpenQA.Selenium;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+
+
+namespace APIrequestJSON
+{
+    public static class APIhelper
+    {
+        public static IRestResponse SendAPIrequest(object body, Dictionary<string, string> headers, string link, Method type)
+        {
+            RestClient client = new RestClient(link)
+            {
+                Timeout = 300000
+            };
+
+            RestRequest request = new RestRequest(type);
+            foreach (var header in headers)
+                request.AddHeader(header.Key, header.Value);
+            bool isBodyJson = false;
+            foreach (var header in headers)
+            {
+                if (header.Value.Contains("application/json"))
+                {
+                    isBodyJson = true;
+                    break;
+                }
+            }
+            if (!isBodyJson)
+            {
+                foreach (var data in (Dictionary<string, string>)body)
+                {
+                    request.AddParameter(data.Key, data.Value);
+                }
+            }
+            else
+            {
+                request.AddJsonBody(body);
+                request.RequestFormat = DataFormat.Json;
+            }
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+        public static Cookie ExtractCookie(IRestResponse response, string cookieName)
+        {
+            Cookie res = null;
+            foreach (var cookie in response.Cookies)
+                if (cookie.Name.Equals(cookieName))
+                    res = new Cookie(cookie.Name, cookie.Value, cookie.Domain, cookie.Path, null);
+            return res;
+        }
+
+        public static List<Cookie> ExtractAllCookies(IRestResponse response)
+        {
+            List<Cookie> res = new List<Cookie>();
+            foreach (var cookie in response.Cookies)
+                res.Add(new Cookie(cookie.Name, cookie.Value, cookie.Domain, cookie.Path, null));
+            return res;
+        }
+        public static IRestResponse UpLoadImgAPIrequest(object body, Dictionary<string, string> headers, string link, Method type)
+        {
+            RestClient client = new RestClient(link)
+            {
+                Timeout = 300000
+            };
+            RestRequest request = new RestRequest(type);
+            foreach (var header in headers)
+                request.AddHeader(header.Key, header.Value);
+
+            request.AddJsonBody(body);
+            request.RequestFormat = DataFormat.Json;
+
+            IRestResponse response = client.Execute(request);
+
+            return response;
+        }
+    }
+}
---
+++ APIrequestJSON/UnitTest1.cs
@@ -0,0 +1,75 @@
+using System;
+using Xunit;
+using RestSharp;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace APIrequestJSON
+{
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void TestUsingCookies()
+
+        {
+            var headers = new Dictionary<string, string>
+            {
+                { "Content-Type", "application/x-www-form-urlencoded" }
+            };
+            var body = new Dictionary<string, string>
+            {
+                 {"ulogin", "art1613122" },
+                 {"upassword", "505558545 " }
+
+            };
+            var response = APIhelper.SendAPIrequest(body, headers, "https://my.soyuz.in.ua/", Method.POST);
+
+            var cookie = APIhelper.ExtractCookie(response, "zbs_lang");
+            var cookie2 = APIhelper.ExtractCookie(response, "ulogin");
+            var cookie3 = APIhelper.ExtractCookie(response, "upassword");
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://my.soyuz.in.ua/");
+            System.Threading.Thread.Sleep(3000);
+            
+            driver.Manage().Cookies.AddCookie(cookie);
+            driver.Manage().Cookies.AddCookie(cookie2);
+            driver.Manage().Cookies.AddCookie(cookie3);
+            
+
+            driver.Navigate().GoToUrl("https://my.soyuz.in.ua/index.php");
+            System.Threading.Thread.Sleep(10000);
+            driver.Close();
+        }
+        [Fact]
+        public void TestUpLoadImg()
+        {
+            var headers = new Dictionary<string, string>
+            {
+                { "Content-Type", "multipart/form-data" }
+            };
+            var body = new Dictionary<string, object>
+            {
+                {"login", "jofafeg989@nahetech.com" },
+                {"password", "123456789" },
+                 {"avatar", "C:\\Users\\Пользователь\\source\\repos\\APIrequest\\venus.jpg" }
+            };
+
+            var response = APIhelper.UpLoadImgAPIrequest(body, headers, "http://users.bugred.ru/tasks/rest/addavatar/", Method.POST);
+        }
+        [Fact]
+        public void TestDownLoadFile()
+        {
+            RestClient client = new RestClient("https://i.pinimg.com/originals/15/5a/17/155a1725ed188934f0c70617e2b70f46.jpg");
+            var requestImg = new RestRequest(Method.GET);
+            byte[] result = client.DownloadData(requestImg);
+            File.WriteAllBytes(Path.Combine("C:\\Users\\Пользователь\\source\\repos\\APIrequestJSON\\img.jpg"), result);
+        }
+        
+    }
+}
